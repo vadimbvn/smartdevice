70076d7baefcf437dc383f0a6fd7fa3ed682db90
vadimbvn <Sky4nii@yandex.com>
2020-08-19 02:02:35 +0300

    v.2

--- gulpfile.js
+++ gulpfile.js
@@ -23,6 +23,7 @@ gulp.task('css', function () {
       .pipe(sourcemap.init())
       .pipe(sass())
       .pipe(postcss([autoprefixer()]))
+      .pipe(gulp.dest('build/css'))
       .pipe(csso())
       .pipe(rename('style.min.css'))
       .pipe(sourcemap.write('.'))
